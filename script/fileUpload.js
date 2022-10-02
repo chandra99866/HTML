844d88086f5bad02ef9e60dcd897fddd304c810f
Chandrasekhar.R <chandrasekhar.r@molecularconnections.com>
2022-10-02 08:43:17 +0530

    added null file check and removed the unwanted text display

--- script/fileUpload.js
+++ script/fileUpload.js
@@ -1,5 +1,17 @@
 async function uploadFile(){
+  
+
+
+
     const file = document.getElementById("file").files;
+
+
+    if(file[0]===undefined){
+        fileStatus.style.color="red";
+        fileStatus.innerHTML='No file chosen';
+    }
+    else{
+
     const fd = new FormData();
     fd.append('file', file[0]);
     const response = await fetch('http://192.168.1.10:8080/restfull/first/saveFile',{
@@ -11,11 +23,12 @@ async function uploadFile(){
     
     if(response.status==200){
       fileStatus.style.color="green";
-      fileStatus.innerHTML= "uploaded successfuly ...";
+      fileStatus.innerHTML= data.result;
     }
     else{
         fileStatus.style.color="red";
         fileStatus.innerHTML=data.error;
     }
+}
 
 }
