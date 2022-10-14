f03e48413f72b67d5082d354c0c6f01a52e426cf
chandrasekhar.r <chandrasekhar8050@gmail.com>
2022-10-14 07:50:03 +0530

    added catch for file uplod spinner

--- script/fileUpload.js
+++ script/fileUpload.js
@@ -11,18 +11,27 @@ async function uploadFile(){
         fileStatus.innerHTML='No file chosen';
     }
     else{
+
+
    
     document.getElementById("loading").className="spinner-border spinner-border-sm";
     document.getElementById("status").innerHTML="uploading...";
+    let fileStatus =document.getElementById('fileStatus');
 
     const fd = new FormData();
     fd.append('file', file[0]);
     const response = await fetch('http://192.168.1.10:8080/restfull/first/saveFile',{
         method:'POST',
         body:fd
+    }).catch((error)=>{
+        document.getElementById("loading").className="";
+        document.getElementById("status").innerHTML="upload";
+        fileStatus.style.color="red";
+        fileStatus.innerHTML='unable to upload file';
+
     });
     const data = await response.json();
-    let fileStatus =document.getElementById('fileStatus');
+   
     
     if(response.status==200){
     document.getElementById("loading").className="";
